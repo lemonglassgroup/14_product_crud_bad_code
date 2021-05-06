b04c516415acd5b05b364022845f0eb393e0c8cc
lemoncube1 <lemonglasgroup@protonmail.com>
2021-05-06 17:54:36 +0300

    good

---
+++ 03_good/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9ff0d05aaa0eb2436b237693521c3f63fb09c0ff',
+    'name' => 'lemoncube1/03_good',
+  ),
+  'versions' => 
+  array (
+    'lemoncube1/03_good' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9ff0d05aaa0eb2436b237693521c3f63fb09c0ff',
+    ),
+  ),
+);
